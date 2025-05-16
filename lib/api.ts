bc123aaddf638dae41e231f4facb01d6386b8f86
dev-ashir768 <techashir167@gmail.com>
2025-05-16 11:51:58 +0500

    production level packages install

--- lib/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-export const fetchProducts = async () => {
-  try {
-    const res = await axios.get("https://fakestoreapi.com/products");
-    return res.data;
-  } catch (error) {
-    throw new Error("Failed to fetch products");
-  }
-};
